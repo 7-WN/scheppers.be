eec1507f487f2d7c337df42b439e909b6aefb326
Lars De Richter <lars@anansi.be>
2016-07-17 17:11:53 +0200

    logo in the navbar fades in and out when scrolling

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
+    // Animated text in the box on the introduction section
     $("#id-intro__text--replace").Morphext({
         animation: "flipInX"
     });
+
+    // Fade in the site-header
     $("#id-site-header").delay(1500).fadeIn("slow");
+
+    // Scroll events
+    var scherm = $(window);
+    scherm.scroll(function() {
+        if (scherm.scrollTop() > (scherm.height() / 2 - 90)) {
+            $("#id-site-header__logo").fadeIn("slow");
+        } else if (scherm.scrollTop() < (scherm.height() / 2 - 90)) {
+            $("#id-site-header__logo").fadeOut("slow");
+        }
+    });
 });
