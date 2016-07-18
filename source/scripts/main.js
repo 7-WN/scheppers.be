8ef2e521be432cfedb561b01a6ae189306529195
Lars De Richter <lars@anansi.be>
2016-07-18 16:05:02 +0200

    trying for background animation, not working as well as I would wish

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -10,6 +10,17 @@ $(document).ready(function() {
     // Fade in the site-header
     $("#id-site-header").delay(1500).fadeIn("slow");
 
+    // Change the background-images
+    var backgroundCounter = 0;
+    var backgroundImages = ["images/sancta-maria.jpg", "images/deurne.jpg"];
+    var backgroundTimer = setInterval(function() {
+        $("#id-introduction").css("background-image", "linear-gradient(rgba(0, 0, 0, .6),rgba(0, 0, 0, .6)),url(" + backgroundImages[backgroundCounter] + ")", "animation-name", "animatedBackground", "animation-duration", "3000ms");
+        backgroundCounter++;
+        if (backgroundCounter >= backgroundImages.length) {
+            backgroundCounter = 0;
+        }
+    }, 4000);
+
     // Scroll events
     var scherm = $(window);
 
