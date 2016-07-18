d184effa00a8d42c6a0df0f62e1bf3044f6397e0
Lars De Richter <lars@anansi.be>
2016-07-18 14:54:45 +0200

    Secondary nav-bar works (more or less)

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    // Hide all secondary nav-bars
+    $(".nav-bar--secondary").hide();
+
     // Animated text in the box on the introduction section
     $("#id-intro__text--replace").Morphext({
         animation: "flipInX"
@@ -9,11 +12,25 @@ $(document).ready(function() {
 
     // Scroll events
     var scherm = $(window);
+
+    // show the logo on the main nav-bar, when it is not visible in the main window anymore
     scherm.scroll(function() {
         if (scherm.scrollTop() > (scherm.height() / 2 - 90)) {
-            $("#id-site-header__logo").fadeIn("slow");
+            $("#id-site-header__logo").fadeIn();
         } else if (scherm.scrollTop() < (scherm.height() / 2 - 90)) {
-            $("#id-site-header__logo").fadeOut("slow");
+            $("#id-site-header__logo").fadeOut();
         }
+    });
+
+    // Show the correct secondary nav-bar when a link on the main nav-bar is clicked
+    $(".nav-bar__item-link--primary").click(function() {
+        var link = $(this);
+        $(".nav-bar__item-link--primary").removeClass("nav-bar__item-link--active");
+        link.addClass("nav-bar__item-link--active");
+        if (link.data("nav-bar--secondary")) {
+            $(".nav-bar--secondary").hide();
+            $("#" + link.data("nav-bar--secondary")).fadeIn("slow");
+        }
+        return true;
     });
 });
