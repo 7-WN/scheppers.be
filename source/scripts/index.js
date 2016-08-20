47fbb0f015bf48ff7a26d74dff69801ffb927fd3
Lars De Richter <lars@anansi.be>
2016-08-20 19:50:32 +0200

    Navbar does not dissapear when coming from an internal page

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,7 +1,33 @@
-$(document).ready(function() {
-    // Hide secondary nav-bar on home page
-    $("#id-nav-bar--school").hide();
+// Parsing url params see: http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
 
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+// This is where shit gets done
+$(document).ready(function() {
+    // Hide and show the header on the landing page when not arriving from an internal link
+    if (!getUrlParameter("internal")) { // only if we weren't on the site before
+        // Hide site-header
+        $("#id-site-header").hide();
+        // Hide secondary nav-bar on home page
+        $("#id-nav-bar--school").hide();
+        // Fade in the site-header after a delay
+        $("#id-site-header").delay(1500).fadeIn("slow");
+    } else { // when we arrive from an internal link
+        $("#id-nav-bar__item--school").addClass("nav-bar__item-link--active"); // make the button active
+        $("#id-site-header__logo").show();
+    }
     // Show the secondary nav-bar on the home page when the link is clicked
     $("#id-nav-bar__item--school").click(function() {
         $("#id-nav-bar--school").fadeIn("slow");
@@ -13,9 +39,6 @@ $(document).ready(function() {
     $("#id-intro__text--replace").Morphext({
         animation: "flipInX"
     });
-
-    // Fade in the site-header
-    $("#id-site-header").delay(1500).fadeIn("slow");
 
     // Change the background-images
     changeBackgrounds = function() {
@@ -42,9 +65,7 @@ $(document).ready(function() {
     scherm.scroll(function() {
         if (scherm.scrollTop() > (scherm.height() / 2 - 90)) {
             $("#id-site-header__logo").fadeIn();
-            clearInterval(bgTimer); // stop changing the backgroundimages in the intro
-        } else if (scherm.scrollTop() < (scherm.height() / 2 - 90)) {
-            $("#id-site-header__logo").fadeOut();
+            clearInterval(bgTimer); // stop changing the backgroundimages in the after we have scrolled a bit
         }
     });
 });
